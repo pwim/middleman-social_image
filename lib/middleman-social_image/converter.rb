38aa69afa76cf0b79ea9ff1f2b2b295cc9694e7f
Paul McMahon <paul@tokyodev.com>
2020-05-20 15:17:38 +0900

    [WIP] Generate resource on demand

---
+++ lib/middleman-social_image/converter.rb
@@ -0,0 +1,26 @@
+require "middleman-social_image/capybara"
+class Middleman::SocialImage::Converter
+  def initialize(app, window_size)
+    @app = app
+    @window_size = window_size
+  end
+
+  def convert(resource)
+    image_path = File.join(@app.root, "tmp/middleman-social_image.png")
+    FileUtils.mkdir_p(File.dirname(image_path))
+    session.visit(resource.url)
+    session.save_screenshot(image_path)
+    File.read(image_path)
+  end
+
+  private
+
+  def session
+    @session ||= begin
+      rack_app = ::Middleman::Rack.new(@app).to_app
+      Capybara::Session.new(:selenium_chrome_headless, rack_app).tap do |session|
+        session.current_window.resize_to(*@window_size.split(","))
+      end
+    end
+  end
+end
--- lib/middleman-social_image/extension.rb
+++ lib/middleman-social_image/extension.rb
@@ -1,10 +1,32 @@
 require 'middleman-core'
 require 'middleman-social_image/commands'
+require "middleman-social_image/resource"
 
 class Middleman::SocialImage::Extension < ::Middleman::Extension
   option :window_size, '1200,600'
   option :base_asset_dir, 'assets/images/social-images'
-  option :social_image_url_pattern, %r{(/social-image)/$}
-  option :social_image_url_substitution, '\1.png'
+  option :social_image_path_pattern, %r{social-image.html$}
+  option :social_image_path_substitution, 'social-image.png'
   option :selector, "body > *"
+
+  def initialize(app, options_hash={}, &block)
+    super
+    require "middleman-social_image/converter"
+    @converter = Middleman::SocialImage::Converter.new(app, options.window_size)
+  end
+
+  def manipulate_resource_list(resources)
+    social_image_sources = resources.
+      select {|resource| resource.path =~ options.social_image_path_pattern}
+    social_image_resources = social_image_sources.map do |resource|
+      path = resource.path.sub(options.social_image_path_pattern, options.social_image_path_substitution)
+      Middleman::SocialImage::Resource.new(
+        @app.sitemap,
+        path,
+        @converter,
+        resource
+      )
+    end
+    resources + social_image_resources
+  end
 end
