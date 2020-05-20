34662c85dedd5c57d3734f4b29ea0b04a8301102
Paul McMahon <paul@tokyodev.com>
2020-05-20 19:52:33 +0900

    Cont

--- lib/middleman-social_image/converter.rb
+++ lib/middleman-social_image/converter.rb
@@ -1,14 +1,16 @@
 require "middleman-social_image/capybara"
 class Middleman::SocialImage::Converter
-  def initialize(app, window_size)
+  def initialize(app, window_size, selector)
     @app = app
     @window_size = window_size
+    @selector = selector
   end
 
-  def convert(source_url)
+  def convert(url)
+    session.visit(url)
+    raise "#{url} did not contain '#{@selector}'." unless session.has_selector?(@selector)
     image_path = File.join(@app.root, "tmp/middleman-social_image.png")
     FileUtils.mkdir_p(File.dirname(image_path))
-    session.visit(source_url)
     session.save_screenshot(image_path)
     File.read(image_path)
   end
--- lib/middleman-social_image/extension.rb
+++ lib/middleman-social_image/extension.rb
@@ -1,32 +1,27 @@
 require 'middleman-core'
-require 'middleman-social_image/commands'
-require "middleman-social_image/resource"
 
 class Middleman::SocialImage::Extension < ::Middleman::Extension
   option :window_size, '1200,600'
-  option :base_asset_dir, 'assets/images/social-images'
-  option :social_image_path_pattern, %r{social-image.html$}
-  option :social_image_path_substitution, 'social-image.png'
   option :selector, "body > *"
 
   def initialize(app, options_hash={}, &block)
     super
     require "middleman-social_image/converter"
-    @converter = Middleman::SocialImage::Converter.new(app, options.window_size)
+    require "middleman-social_image/resource"
+    @converter = Middleman::SocialImage::Converter.new(app, options.window_size, options.selector)
   end
 
   def manipulate_resource_list(resources)
-    social_image_sources = resources.
-      select {|resource| resource.path =~ options.social_image_path_pattern}
-    social_image_resources = social_image_sources.map do |resource|
-      path = resource.path.sub(options.social_image_path_pattern, options.social_image_path_substitution)
-      Middleman::SocialImage::Resource.new(
-        @app.sitemap,
-        path,
-        @converter,
-        resource
-      )
-    end
-    resources + social_image_resources
+    resources + resources.
+      select {|resource| resource.options[:social_image] }.
+      map do |resource|
+        path = resource.path.sub(".html", ".png")
+        Middleman::SocialImage::Resource.new(
+          @app.sitemap,
+          path,
+          @converter,
+          resource
+        )
+      end
   end
 end
