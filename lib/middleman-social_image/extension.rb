7f7c403721467aa8599bdee771d7c7052fb88c07
Paul McMahon <paul@tokyodev.com>
2020-05-21 16:16:26 +0900

    Continue

--- lib/middleman-social_image/extension.rb
+++ lib/middleman-social_image/extension.rb
@@ -4,6 +4,7 @@ class Middleman::SocialImage::Extension < ::Middleman::Extension
   option :window_size, '1200,600'
   option :selector, "body > *"
   option :always_generate, true
+  option :parallel, true
 
   def initialize(app, options_hash={}, &block)
     super
@@ -12,40 +13,51 @@ class Middleman::SocialImage::Extension < ::Middleman::Extension
     @converter = Middleman::SocialImage::Converter.new(app, options.window_size, options.selector, options.always_generate)
   end
 
-  def before_build(builder)
-    build_resource = proc do |resource|
-      @converter.convert(resource)
-    end
-    if builder.instance_variable_get(:@parallel)
-      ::Parallel.each(social_image_resources, &build_resource)
-    else
-      social_image_resources.each(&build_resource)
-    end
-  end
-
   def manipulate_resource_list(resources)
-    resources + social_image_resources.map do |resource|
-      path = resource.path.sub(".html", ".png")
-      if app.build?
-        Middleman::Sitemap::Resource.new(
-          @app.sitemap,
-          path,
-          @converter.image_path(resource)
-        )
-      else
-        Middleman::SocialImage::Resource.new(
-          @app.sitemap,
-          path,
-          @converter,
-          resource
-        )
-      end
+    if app.build?
+      manipulate_resource_list_for_build(resources)
+    else
+      manipulate_resource_list_for_preview(resources)
     end
   end
 
   private
 
-  def social_image_resources
+  def social_image_source_resources
     app.sitemap.resources.select {|resource| resource.options[:social_image] || resource.data[:social_image] }
+  end
+
+  def path_for_source_resource(resource)
+    resource.path.sub(".html", ".png")
+  end
+
+  def manipulate_resource_list_for_build(resources)
+    convert_resource = proc do |resource|
+      @converter.convert(resource)
+    end
+    if options.parallel
+      ::Parallel.each(social_image_source_resources, &convert_resource)
+    else
+      social_image_source_resources.each(&convert_resource)
+    end
+    social_image_resources = social_image_source_resources.map do |resource|
+      Middleman::Sitemap::Resource.new(
+        @app.sitemap,
+        path_for_source_resource(resource),
+        @converter.image_path(resource)
+      )
+    end
+    resources + social_image_resources - social_image_source_resources
+  end
+
+  def manipulate_resource_list_for_preview(resources)
+    resources + social_image_source_resources.map do |resource|
+      Middleman::SocialImage::Resource.new(
+        @app.sitemap,
+        path,
+        @converter,
+        path_for_source_resource(resouce)
+      )
+    end
   end
 end
